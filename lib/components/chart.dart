96fead4538c451eb257a8eb3f538380ddd7c80f7
thiago-anjos <thiago.w3c@gmail.com>
2021-06-20 10:52:13 -0300

    chart

--- lib/components/chart.dart
+++ lib/components/chart.dart
@@ -31,19 +31,33 @@ class Chart extends StatelessWidget {
 
   Chart(this.recentTransaction);
 
+  double get _weekTotalValue {
+    return groupedTransactions.fold(0.0, (sum, tr) {
+      double value = tr['value'] as double;
+      return sum + value;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 6,
       margin: EdgeInsets.all(20),
-      child: Row(
-        children: groupedTransactions.map((tr) {
-          return ChartBar(
-            label: tr['day'] as String,
-            value: tr['value'] as double,
-            percentage: 0,
-          );
-        }).toList(),
+      child: Padding(
+        padding: const EdgeInsets.all(10),
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceAround,
+          children: groupedTransactions.map((tr) {
+            return Flexible(
+              fit: FlexFit.tight,
+              child: ChartBar(
+                label: tr['day'] as String,
+                value: tr['value'] as double,
+                percentage: (tr['value'] as double) / _weekTotalValue,
+              ),
+            );
+          }).toList(),
+        ),
       ),
     );
   }
--- lib/components/chart_bar.dart
+++ lib/components/chart_bar.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ChartBar extends StatelessWidget {
   final String label;
@@ -14,14 +15,33 @@ class ChartBar extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(children: [
-      Text('r\$${value.toStringAsFixed(2)}'),
+      FittedBox(
+          child: Text(
+              '${value.toStringAsFixed(2)}')), //o FittedBox diminui o tamanho do texto para caber dentro do box
       SizedBox(
         height: 5,
       ),
       Container(
-        height: 60,
+        height: 100,
         width: 10,
-        child: null,
+        child: Stack(
+          alignment: Alignment.bottomCenter,
+          children: <Widget>[
+            Container(
+                decoration: BoxDecoration(
+              border: Border.all(color: Colors.grey, width: 1.0),
+              color: Color.fromRGBO(220, 220, 220, 1),
+              borderRadius: BorderRadius.circular(5),
+            )),
+            FractionallySizedBox(
+                heightFactor: percentage,
+                child: Container(
+                  decoration: BoxDecoration(
+                    color: Theme.of(context).primaryColor,
+                  ),
+                ))
+          ],
+        ),
       ),
       SizedBox(
         height: 5,
